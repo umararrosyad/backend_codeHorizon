c0926fb0a2fe7deefdeb537d594afa8c2316157b
Randy Umar Arrosyad <78028744+umararrosyad@users.noreply.github.com>
2024-02-09 00:00:42 +0800

    feedback route

--- routes/feedbacks.js
+++ routes/feedbacks.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const FeedbackController = require("../controllers/feedbackController");
-// const auth = require('../middleware/auth')
+const multer = require('../middleware/multer')
 
 router.get("/:product_id/feedback/", FeedbackController.getAll);
 router.get("/:product_id/feedback/:id", FeedbackController.getOne);
-router.post("/:product_id/feedback/", FeedbackController.create);
+router.post("/:product_id/feedback/",multer.array("image"), FeedbackController.create);
 router.put("/:product_id/feedback/:id", FeedbackController.update);
 router.delete("/:product_id/feedback/:id", FeedbackController.delete);
+router.delete("/:product_id/feedback/gallery/:id", FeedbackController.deleteGallery);
 
 module.exports = router;
