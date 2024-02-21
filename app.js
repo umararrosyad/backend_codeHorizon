621e8526e19132f773e19046b30bcc0addd16ea0
Randy Umar Arrosyad <78028744+umararrosyad@users.noreply.github.com>
2024-02-21 22:04:39 +0800

    add cors and cange port

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const cors = require('cors');
+const port = 4000
 const router = require('./routes')
 const errorHandler = require('./middleware/errorHandler')
 
 app.use(express.json())
+app.use(cors());
 app.use(express.urlencoded({extended : true}))
 
 app.use("/api/v1",router)
